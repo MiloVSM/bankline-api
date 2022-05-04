547bfe8918c277089d4e885bf1f0792a3725b1ab
Murilo Vieira <milo.wizard@outlook.com>
2022-05-04 10:37:19 -0300

    Entity Mapping with JPA
    Mapeamento das entidades com JPA

--- src/main/java/com/dio/santander/banklineapi/model/Conta.java
+++ src/main/java/com/dio/santander/banklineapi/model/Conta.java
@@ -1,9 +1,15 @@
 package com.dio.santander.banklineapi.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Conta {
+	@Column(name = "conta_numero")
 	private Long numero;
-	private Double saldo;
 	
+	@Column(name = "conta_saldo")
+	private Double saldo;
 	public Long getNumero() {
 		return numero;
 	}
