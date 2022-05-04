76e7182948f16437096779e2756991b2fa139a45
Murilo Vieira <milo.wizard@outlook.com>
2022-05-04 14:24:04 -0300

    v1.0.0

--- src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
+++ src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
@@ -23,6 +23,9 @@ public class Movimentacao {
 	private Double valor;
 	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
+	
+	@Column(name = "id_conta")
+	private Integer idConta;
 	public Integer getId() {
 		return id;
 	}
@@ -52,6 +55,12 @@ public class Movimentacao {
 	}
 	public void setTipo(MovimentacaoTipo tipo) {
 		this.tipo = tipo;
+	}
+	public Integer getIdConta() {
+		return idConta;
+	}
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
 	}
 	
 }
